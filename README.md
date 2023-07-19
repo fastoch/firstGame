7f6d839c7c701809334e3055a30ab31f1f5a4192
fastoch <fastoch@ik.me>
2023-07-19 17:16:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## JavaScript Full Course by Dave Gray  
+https://youtu.be/EfAl9bwzVZk
+
+TIMELINE: Chapter 11 - @1:13:00
---
+++ js/main.js
@@ -0,0 +1,29 @@
+// First interactive game
+window.onload = function game() {
+  let playGame = confirm("Shall we play rock, paper, or scissors?");
+  if (playGame) {
+    let playerChoice = prompt("Please enter rock, paper, or scissors:");
+    if (playerChoice) {
+      let playerOne = playerChoice.trim().toLowerCase();
+      if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
+        let computerChoice = Math.floor(Math.random() * 3 + 1);
+        let computer = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors";
+
+        let result = playerOne === computer ? "Tie game!"
+          : playerOne === "rock" && computer === "paper" ?
+            `player: ${playerOne}\ncomputer: ${computer}\nComputer wins!`
+            : playerOne === "paper" && computer === "scissors" ?
+              `player: ${playerOne}\ncomputer: ${computer}\nComputer wins!`
+              : playerOne === "scissors" && computer === "rock" ? "Computer wins!"
+                : `player: ${playerOne}\ncomputer: ${computer}\nPlayer wins!`;
+        alert(result);
+      } else {
+        alert("You didn't enter rock, paper, or scissors.");
+      }
+    } else {
+      alert("I guess you changed your mind. Maybe next time...");
+    }
+  } else {
+    alert("OK, maybe next time...");
+  }
+}
