3f160d6083b9e5bfe67bcbf106aa17b6e14ef2e5
fastoch <fastoch@ik.me>
2023-07-19 17:53:47 +0200

    adding play again feature

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ window.onload = function game() {
     if (playerChoice) {
       let playerOne = playerChoice.trim().toLowerCase();
       if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
+
         let computerChoice = Math.floor(Math.random() * 3 + 1);
         let computer = computerChoice === 1 ? "rock" : computerChoice === 2 ? "paper" : "scissors";
 
@@ -17,6 +18,10 @@ window.onload = function game() {
               : playerOne === "scissors" && computer === "rock" ? "Computer wins!"
                 : `player: ${playerOne}\ncomputer: ${computer}\nPlayer wins!`;
         alert(result);
+
+        let playAgain = confirm("Would you like to play again?");
+        playAgain ? location.reload() : alert("Ok, thanks for playing.");
+        // location.reload() reloads the web page 
       } else {
         alert("You didn't enter rock, paper, or scissors.");
       }
